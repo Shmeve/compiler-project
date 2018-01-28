f57ea970276d2d0e19d0708c2fc67ef65d5dd9a4
Steve Ferreira <sjferreira516@gmail.com>
2018-01-28 14:33:08 -0500

    Make state transition table a list. Fixes #1

--- compiler/scanner/transition_table.py
+++ compiler/scanner/transition_table.py
@@ -3,19 +3,19 @@ import csv
 
 
 class TransitionTable:
-    def __init__(self, csv_path="compiler/tools/state_transition_table.csv"):
+    def __init__(self, csv_path: str="compiler/tools/state_transition_table.csv"):
         """
         Constructor for TransitionTable object.
 
-        :param csv_path: location of csv file to use when constructing the state transition table.
+        :param csv_path: str, location of csv file to use when constructing the state transition table.
         """
         self.csv_path: str = csv_path
-        self.table: dict = OrderedDict()
+        self.table: list = []
 
     def build_table(self) -> None:
         """
-        Create state transition table from CSV. The table is represented as a Dictionary of Dictionaries where the state
-        to transition to can be accessed via table[currentState: str][inputKey: str].
+        Create state transition table from CSV. The table is represented as a List of Dictionaries where the state
+        to transition to can be accessed via table[currentState: int][inputKey: str].
 
         :return: None
         """
@@ -23,6 +23,7 @@ class TransitionTable:
             lines = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
 
         keys = lines[0]
+        self.table.append(keys)
 
         # Set up table keys
         for i in range(1, len(lines)):
@@ -31,4 +32,4 @@ class TransitionTable:
             for j in range(len(keys)):
                 row[keys[j]] = state_transitions[j]
 
-            self.table[str(i)] = row
+            self.table.append(row)
