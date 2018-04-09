6978838b25cff955ef3449d40d36fdf7cbe76983
Steve Ferreira <sjferreira516@gmail.com>
2018-04-09 14:05:22 -0400

    symbol table helper methods

--- compiler/datastructures/symbol_table.py
+++ compiler/datastructures/symbol_table.py
@@ -1,24 +1,46 @@
+class SymbolTableElement:
+    def __init__(self, element_name: str="", element_kind: str="", element_type: str="", element_link=None):
+        self.element_name = element_name
+        self.element_kind = element_kind
+        self.element_type = element_type
+        self.element_link: SymbolTable = element_link
+
+
 class SymbolTable:
     def __init__(self, table_name: str="", symbols: list=list(), table_size: int=0, table_level: int=0, parent_table=None):
         self.name = table_name
         self.symbols = symbols
         self.table_size = table_size
         self.table_level = table_level
-        self.parent_table = parent_table
+        self.parent_table: SymbolTable = parent_table
 
-    def insert(self):
-        pass
+    def insert(self, element: SymbolTableElement):
+        """
+        Add element to symbol table
 
-    def search(self):
-        pass
+        :param element: SymbolTableElement to add
+        :return: SymbolTableElement
+        """
+        self.symbols.append(element)
+
+    def search(self, search_element_name: str) -> SymbolTableElement:
+        """
+        Look for element in current table or preceding tables
+
+        :param search_element_name: name of element being searched for
+        :return: SymbolTableElement (Default null Element returned if not found)
+        """
+        found: bool = False
+
+        for element in self.symbols:
+            if element.element_name == search_element_name:
+                found = True
+                return element
+
+        if not found and self.parent_table is not None:
+            return self.parent_table.search(search_element_name)
+
+        return SymbolTableElement()
 
     def delete(self):
         pass
-
-
-class SymbolTableElement:
-    def __init__(self, element_name: str="", element_kind: str="", element_type: str="", element_link: SymbolTable=None):
-        self.element_name = element_name
-        self.element_kind = element_kind
-        self.element_type = element_type
-        self.element_link = element_link
